e7448326dfb3240465b1eeb7e46906bf34f2de1e
Glenn Olazo <glennolazo@gmail.com>
2020-05-29 19:22:20 +0800

    Parent-Child-Component Implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { SiblingComponent } from './sibling/sibling.component';
+import { CharacterDataService } from './services/character-data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent,
+    SiblingComponent
   ],
   imports: [
     BrowserModule
