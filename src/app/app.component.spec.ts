574118ca7f2fe423c8e37fa1e7119a90c9897033
Glenn Olazo <glennolazo@gmail.com>
2020-09-22 22:57:20 +0800

    Updates for deploying app to heroku

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'raksquad-demo-app'`, () => {
+  it(`should have as title 'lcb-rms-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('raksquad-demo-app');
+    expect(app.title).toEqual('lcb-rms-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('raksquad-demo-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('lcb-rms-app app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'raksquad-demo-app';
+  title = 'lcb-rms-app';
 }
